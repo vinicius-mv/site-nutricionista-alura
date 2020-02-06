60fc2a8ca1ff026829bdbc54549cec181ca3c566
Vinicius Vieira <viniciusmv91@gmail.com>
2020-02-06 00:09:39 -0300

    ajuste timeout efeito fadeout de remocao da linha da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -4,10 +4,10 @@ tabela.addEventListener("dblclick", function (event) {
     if (event.target.textContent == "⌦") {
 
         event.target.parentNode.classList.add("fadeOut"); //adiciona class com efeito de opacidade que dura 500ms
-        setInterval(function () { //função que remove após o efeito de 500ms 
+        setTimeout(function () { //função que remove após o efeito de 500ms 
             event.target.parentNode.remove(); //event.target  ==  <td>       event.target.parentNode = <tr>
         }, 500);
     }
-})
+});
 
 
